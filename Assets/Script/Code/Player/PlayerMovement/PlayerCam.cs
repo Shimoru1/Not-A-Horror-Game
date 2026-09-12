45fdecaf916eb368ca1721f50469556b101fd279
SMF007hehe <sasinipha.mar@spumail.net>
2026-09-12 21:18:23 +0700

    เพิ่ม scene / โฟลเดอร์ code ของแต่ละแผนก
    เพิ่ม scene กับ โฟลเดอร์ code ของแต่ละแผนกคับฟู่ว

---
+++ Assets/Script/Code/Player/PlayerMovement/PlayerCam.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+	public float fov = 90f;
+
+	public Transform orientaion;
+
+    float xRotation;
+    float yRotation;
+
+	private Camera cam;
+
+	private void Start()
+    {
+		cam = GetComponent<Camera>();
+		cam.fieldOfView = fov;
+
+		Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false; 
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("MouseX") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("MouseY") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);//กันคอบิด
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientaion.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
